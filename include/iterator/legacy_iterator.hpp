21495cfe466ce1aa2f96d106d5b87869b30fab6c
Kirill Chernigovskiy <138433730+kcherno@users.noreply.github.com>
2024-06-26 17:01:42 +0800

    Add: clarification to requirements

--- include/iterator/legacy_iterator.hpp
+++ include/iterator/legacy_iterator.hpp
@@ -6,62 +6,66 @@
 
 #include "feature-macros/include/cxx_standard.hpp"
 
+#define LEGACY_ITERATOR_DEPRECATED__
+
+#if __cplusplus >= CXX20_STANDARD__
+#  undef  LEGACY_ITERATOR_DEPRECATED__
+#  define LEGACY_ITERATOR_DEPRECATED__ [[deprecated ("use iterator concepts")]]
+#endif
+
 namespace name_requirement
 {
     namespace iterator
     {
 	template<typename T>
-	class is_legacy_iterator;
+	class LEGACY_ITERATOR_DEPRECATED__ legacy_iterator;
 
 	template<typename T>
-	class is_legacy_iterator<T*>;
+	class legacy_iterator<T*>;
     }
 }
 
 template<typename T>
-class name_requirement::iterator::is_legacy_iterator
+class name_requirement::iterator::legacy_iterator
 {
 public:
-#if   __cplusplus >= CXX20_STANDARD__
-
-    // TODO(#1): add C++20 changes
-
-#elif __cplusplus >= CXX11_STANDARD__
-
     using difference_type   = typename std::iterator_traits<T>::difference_type;
     using value_type        = typename std::iterator_traits<T>::value_type;
     using pointer           = typename std::iterator_traits<T>::pointer;
     using reference         = typename std::iterator_traits<T>::reference;
     using iterator_category = typename std::iterator_traits<T>::iterator_category;
 
-    constexpr static bool value =
-	std::is_copy_constructible<T>::value &&
-	std::is_copy_assignable<T>::value    &&
-	std::is_destructible<T>::value;
+    static_assert(std::is_copy_constructible<T>::value,
+		  "T type must satisfy CopyConstructible");
+
+    static_assert(std::is_copy_assignable<T>::value,
+		  "T type must satisfy CopyAssignable");
+
+    static_assert(std::is_destructible<T>::value,
+		  "T type must satisfy Destructible");
+
+    // TODO(#1): add swappable requirement
+
+    static_assert(std::is_same<typename std::add_lvalue_reference<T>::type,
+		               decltype(std::declval<T>().operator++())
+		               >::value,
+		  "T type must support the pre-increment operator++() "
+		  "and returns lvalue reference T&");
+
+    constexpr static bool value = std::true_type::value;
 
 private:
     auto dereference()
 	-> decltype(std::declval<T>().operator*());
-
-    auto increment()
-	-> decltype(static_cast<T&>(std::declval<T>().operator++()));
-
-#endif
 };
 
 template<typename T>
-class name_requirement::iterator::is_legacy_iterator<T*>
+class name_requirement::iterator::legacy_iterator<T*>
 {
 private:
     using this_type = typename std::add_pointer<T>::type;
     
 public:
-#if   __cplusplus >= CXX20_STANDARD__
-
-    // TODO(#2): add C++20 changes
-
-#elif __cplusplus >= CXX11_STANDARD__
-
     using difference_type =
 	typename std::iterator_traits<this_type>::difference_type;
 
@@ -78,6 +82,4 @@ public:
 	typename std::iterator_traits<this_type>::iterator_category;
 
     constexpr static bool value = std::true_type::value;
-
-#endif
 };
