f7bf607e414e5e3b7df724419a17c3cef5cdaaec
Kirill Chernigovskiy <138433730+kcherno@users.noreply.github.com>
2024-06-27 01:50:14 +0800

    Add: clarification to requirements

--- include/iterator/legacy_iterator.hpp
+++ include/iterator/legacy_iterator.hpp
@@ -4,13 +4,15 @@
 #include <iterator>
 #include <utility>
 
-#include "feature-macros/include/cxx_standard.hpp"
+#include "feature-macros/include/feature_macros.hpp"
+#include "library/swappable.hpp"
 
 #define LEGACY_ITERATOR_DEPRECATED__
 
 #if __cplusplus >= CXX20_STANDARD__
 #  undef  LEGACY_ITERATOR_DEPRECATED__
 #  define LEGACY_ITERATOR_DEPRECATED__ [[deprecated ("use iterator concepts")]]
+
 #endif
 
 namespace name_requirement
@@ -18,16 +20,43 @@ namespace name_requirement
     namespace iterator
     {
 	template<typename T>
-	class LEGACY_ITERATOR_DEPRECATED__ legacy_iterator;
+	class LEGACY_ITERATOR_DEPRECATED__ is_legacy_iterator;
 
 	template<typename T>
-	class legacy_iterator<T*>;
+	class is_legacy_iterator<T*>;
     }
 }
 
 template<typename T>
-class name_requirement::iterator::legacy_iterator
+class name_requirement::iterator::is_legacy_iterator
 {
+private:
+    IMMEDIATE_FUNCTION_SPECIFIER__
+    static auto dereference(int) noexcept
+	-> decltype(std::declval<T>().operator*(), bool {})
+    {
+	return true;
+    }
+
+    IMMEDIATE_FUNCTION_SPECIFIER__
+    static bool dereference(...) noexcept
+    {
+	return false;
+    }
+
+    IMMEDIATE_FUNCTION_SPECIFIER__
+    static auto pre_increment(int) noexcept
+	-> decltype(std::declval<T>().operator++(), bool {})
+    {
+	return true;
+    }
+
+    IMMEDIATE_FUNCTION_SPECIFIER__
+    static bool pre_increment(...) noexcept
+    {
+	return false;
+    }
+
 public:
     using difference_type   = typename std::iterator_traits<T>::difference_type;
     using value_type        = typename std::iterator_traits<T>::value_type;
@@ -44,23 +73,25 @@ public:
     static_assert(std::is_destructible<T>::value,
 		  "T type must satisfy Destructible");
 
-    // TODO(#1): add swappable requirement
+    static_assert(library::is_swappable<T>::value,
+		  "T type must satisfy Swappable");
+
+    static_assert(dereference(0),
+		  "T type must support the dereference operator*()");
+
+    static_assert(pre_increment(0),
+		  "T type must support the pre-increment operator++()");
 
     static_assert(std::is_same<typename std::add_lvalue_reference<T>::type,
 		               decltype(std::declval<T>().operator++())
 		               >::value,
-		  "T type must support the pre-increment operator++() "
-		  "and returns lvalue reference T&");
+		  "The return value of operator++() must be an lvalue reference T&");
 
     constexpr static bool value = std::true_type::value;
-
-private:
-    auto dereference()
-	-> decltype(std::declval<T>().operator*());
 };
 
 template<typename T>
-class name_requirement::iterator::legacy_iterator<T*>
+class name_requirement::iterator::is_legacy_iterator<T*>
 {
 private:
     using this_type = typename std::add_pointer<T>::type;
